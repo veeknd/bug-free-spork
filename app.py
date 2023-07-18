1de8713c8c2e98c5f6668c9fbf9151cfad2e5868
Veeknd <sethumanikkams@gmail.com>
2023-07-18 17:07:18 +0530

    :notebook: added abort func replacing return error messasges

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from db import stores,items
+from flask-smorest import abort
 app = Flask(__name__)
 import uuid
 
@@ -10,6 +11,11 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request, ensure 'name' is included in json payload")
+    for store in stores.values():
+        if store_data['name'] == store['name']:
+            abort(400, message="Bad request, store already exist")
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -19,12 +25,17 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    if ("name" not in item_data or
+        "store_id" not in item_data or
+        "price" not in item_data):
+        abort(400, message="Bad request, Ensure 'store_id' ,'name', 'price' are included in json payload")
+
     if item_data["store_id"] not in stores:
-        return {"message":"store not found"}, 404
-    
+        abort (404, message="store not found")
     item_id = uuid.uuid4().hex
     item = {**item_data, "id":item_id}
     item[item_id]= item
+    return item
 
 @app.get("/item")
 def get_all_items():
@@ -34,5 +45,14 @@ def get_all_items():
 def get_store(item_id):
     try:
         return item[item_id]
-    except KeyError:    
-        return {"message": "store not found"},404
+    except KeyError:
+        abort(404, message ="store not found")
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+
+        return stores[store_id]
+    except KeyError:
+
+        abort(404, message="Store not found.")
